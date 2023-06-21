b56e130bb25138795fa8991b5f03320fe2676a8f
zacht-s <zachtownleysmith@gmail.com>
2023-06-21 11:28:55 -0500

    Added Binomial Tree method for American Option class

--- option_pricers.py
+++ option_pricers.py
@@ -12,7 +12,7 @@ class European:
         self.type = type
 
     def __repr__(self):
-        return 'These are European Options'
+        return f'European {self.type} option. S0:{self.s0}  K:{self.k}  TTM:{self.ttm}  VOL{self.vol}  RF:{self.r}'
         pass
 
     def bsm_price(self):
@@ -31,6 +31,63 @@ class European:
         return round(price, 2)
 
 
+class American:
+    def __init__(self, s0, k, ttm, r, vol, type):
+        self.s0 = s0
+        self.k = k
+        self.ttm = ttm
+        self.r = r
+        self.vol = vol
+        self.type = type
+
+        if self.type.lower() not in ['call', 'put']:
+            raise TypeError('Type Variable must be Call or Put')
+
+    def __repr__(self):
+        return f'American {self.type} option. S0:{self.s0}  K:{self.k}  TTM:{self.ttm}  VOL{self.vol}  RF:{self.r}'
+        pass
+
+    def bin_tree_price(self, steps):
+        dt = self.ttm/steps
+        u = math.exp(self.vol * math.sqrt(dt))
+        d = math.exp(-self.vol * math.sqrt(dt))
+        p = (math.exp(self.r * dt) - d) / (u - d)
+
+        def tree_node(s0, up_exp, dn_exp):
+            # Helper Function
+            node = [round(s0 * (u ** up) * (d ** dn), 4) for up, dn in zip(up_exp, dn_exp)]
+            return node
+
+        # Build Binomial Tree of Asset Prices
+        asset_tree = [self.s0]
+        for i in range(steps):
+            up_pwr = list(range(i+2))
+            dn_pwr = up_pwr[::-1]
+            node = tree_node(s0=self.s0, up_exp=up_pwr, dn_exp=dn_pwr)
+            asset_tree.append(node)
+
+        # Work Backward through Binomial Tree to Calculate Option Price
+        discount = math.exp(-self.r * self.ttm / steps)
+
+        np1_prices = []
+        for i in range(len(asset_tree), 1, -1):
+            if self.type.lower() == 'call':
+                imm_exercise = [max(x - self.k, 0) for x in asset_tree[i-1]]
+            else:
+                imm_exercise = [max(self.k - x, 0) for x in asset_tree[i-1]]
+
+            if len(asset_tree[i-1]) == len(asset_tree):
+                hold_vec = [0]*len(asset_tree)
+            else:
+                hold_vec = [(p * np1_prices[x_up] + (1 - p) * np1_prices[x_dn]) * discount
+                            for x_dn, x_up in zip(range(len(np1_prices) - 1), range(1, len(np1_prices)))]
+
+            np1_prices = [max(x, y) for x, y in zip(hold_vec, imm_exercise)]
+
+        option_price = ((1 - p) * np1_prices[0] + p * np1_prices[1]) * discount
+        return round(option_price, 3)
+
+
 if __name__ == '__main__':
     test1 = European(s0=100, k=110, ttm=0.5, r=0.04, vol=0.3, type='call')
     #print(f'European Call Price: {test1.bsm_price()}')
@@ -38,8 +95,11 @@ if __name__ == '__main__':
     test1 = European(s0=100, k=110, ttm=0.5, r=0.04, vol=0.3, type='Put')
     #print(f'European Put Price: {test1.bsm_price()}')
 
+    test2 = American(s0=50, k=50, ttm=0.4167, r=0.1, vol=0.4, type='Put')
+    #print(f'American Put Price, Binomial Tree: {test2.bin_tree_price(steps=5)}')
 
-
+    test2 = American(s0=50, k=50, ttm=0.4167, r=0.1, vol=0.4, type='call')
+    #print(f'American Call Price, Binomial Tree: {test2.bin_tree_price(steps=5)}')
 
 
 
